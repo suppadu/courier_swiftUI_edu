f53541a41490681e8b0d7be20fa242ac234b3653
Suppadu <dkon1411@gmail.com>
2020-11-11 23:35:08 +0800

    Update ContentView

--- Courier/ContentView.swift
+++ Courier/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 15.0){
+            HStack(alignment: .center, spacing: 100.0) {
+                Text("Стоимость часа:")
+                    .font(.system(size: 25))
+                TextField("70", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 60.0, height: 30.0)
+            }
+            HStack(alignment: .center, spacing: 100.0){
+                Text("Стоимость чека:")
+                    .font(.system(size: 25))
+                TextField("60", text: .constant(""))
+                    .multilineTextAlignment(.center)
+                    .frame(width: 60.0, height: 30.0)
+            }
+            VStack {
+                Text("Количество часов")
+                Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/)
+                    .padding(.horizontal, 25)
+            }
+            VStack {
+                Text("Количество чеков")
+                Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/)
+                    .padding(.horizontal, 25)
+                
+            }
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
